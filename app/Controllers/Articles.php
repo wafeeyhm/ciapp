de36af59d8db9076ad1606275d7ceb3d40e8492b
wafiy@anak.it <wafiy@anak.it>
2024-11-15 17:03:02 +0800

    test db connection

--- app/Controllers/Articles.php
+++ app/Controllers/Articles.php
@@ -7,11 +7,15 @@ class Articles extends BaseController
     public function index() 
     {
 
+        // $db = db_connect();
+
+        // $db->listTables();
+
         $data = [
             ['title' => 'One',
             'content' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Eget rhoncus vitae condimentum nisi adipiscing sollicitudin et vivamus facilisi. Odio nullam eros interdum amet varius nascetur. Nullam ac natoque vivamus lacinia; luctus auctor placerat ullamcorper. Curabitur eget justo finibus suscipit litora conubia diam commodo. Mus arcu senectus mollis curae rutrum nec. Libero tempor nibh venenatis bibendum primis proin vestibulum.'],
             ['title' => 'Two',
-            'content' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Eget rhoncus vitae condimentum nisi adipiscing sollicitudin et vivamus facilisi. Odio nullam eros interdum amet varius nascetur. Nullam ac natoque vivamus lacinia; luctus auctor placerat ullamcorper. Curabitur eget justo finibus suscipit litora conubia diam commodo. Mus arcu senectus mollis curae rutrum nec. Libero tempor nibh venenatis bibendum primis proin vestibulum.']
+            'content' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Eget rhoncus vitae condimentum nisi adipiscing sollicitudin et vivamus facilisi. Odio nullam eros interdum amet varius nascetur. Nullam ac natoque vivamus lacinia; luctus auctor placerat ullamcorper. Curabitur eget justo finibus suscipit litora conubia diam commodo. Mus arcu senectus mollis curae rutrum nec. Libero tempor nibh venenatis bibendum primis proin vestibulum.'],
         ];
 
         return view("Articles/index", [
