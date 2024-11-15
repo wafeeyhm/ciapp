caf17d8edd12478e6dd33fc4415c1d70932bd718
wafiy@anak.it <wafiy@anak.it>
2024-11-15 16:55:52 +0800

    display dynamic data from controller to the view

--- app/Views/Articles/index.php
+++ app/Views/Articles/index.php
@@ -3,6 +3,16 @@
 <?= $this->section("title") ?>Article<?= $this->endSection() ?>
 
 <?= $this->section("content") ?>
+
 <h1>Articles section</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti sapiente illo maiores eum reiciendis ea dolores nesciunt aliquam magnam quasi officia, aut facere in eaque obcaecati eligendi, amet suscipit!</p>
+
+<?php foreach ($articles as $article): ?>
+
+<article>
+    <h2><?= $article["title"] ?></h2>
+    <p><?= $article["content"] ?></p>
+</article>
+
+<?php endforeach; ?>
+
 <?= $this->endSection() ?>
