38c21500c947333e90464e76505982916a7b3dce
Abdul Wafiy Haji Mohammad <wafiy.mohammad@unn.com.bn>
2024-11-15 22:41:49 +0800

    add form with input elements

--- app/Views/Articles/new.php
+++ app/Views/Articles/new.php
@@ -6,4 +6,24 @@
 
 <h1>New Article</h1>
 
+<?= form_open("articles/create") ?>
+
+<div class="mb-3">
+    <label for="title" class="form-label">
+        Title
+    </label>
+    <input type="text" class="form-control" id="title">
+</div>
+<div class="mb-3">
+    <label for="content" class="form-label">
+        content
+    </label>
+    <textarea class="form-control" rows="3" name="content" id="content"></textarea>
+</div>
+<div class="mb-3">
+    <button class="btn btn-primary">Save</button>
+</div>
+
+<?= form_close() ?>
+
 <?= $this->endSection() ?>
